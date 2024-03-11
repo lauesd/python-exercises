0ced028a782941381cb519470d87176ebe475794
Gabriel Organista <organistamunozjose@gmail.com>
2024-03-11 20:40:27 +0100

    Nuevos ejercicios

---
+++ conversor-de-monedas.py
@@ -0,0 +1,14 @@
+# Definir la tasa de cambio de dólares a pesos colombianos
+tasa_cambio_peso_colombiano = 4000
+
+# Solicitar al usuario que ingrese la cantidad de dólares a convertir a pesos colombianos
+cantidad_origen_dolar = int(input("¿Cuántos dólares te gustaría cambiar a pesos colombianos?: "))
+
+# Calcular el valor total en pesos colombianos utilizando la tasa de cambio
+valor_total = cantidad_origen_dolar * tasa_cambio_peso_colombiano
+
+# Convertir el valor total a una cadena para imprimirlo
+valor_total = str(valor_total)
+
+# Imprimir el resultado de la conversión
+print("El valor de tus dólares convertidos a pesos colombianos es de: " + valor_total)
---
+++ operaciones-locas.py
@@ -0,0 +1,35 @@
+# Definición de la variable 'nombre' como una cadena de texto
+nombre = "Alejandro"
+
+# Definición de la variable 'apellido' como una cadena de texto
+apellido = " Torres"
+
+# Imprime la identificación única de la variable 'nombre'
+print(id(nombre))
+
+# Imprime la concatenación de las variables 'nombre' y 'apellido'
+print(nombre + apellido)
+
+# Reasignación de la variable 'nombre' como un entero (12)
+nombre = int(12)
+
+# Imprime la identificación única de la variable 'nombre' después de la reasignación
+print(id(nombre))
+
+# Imprime el valor de la variable 'nombre' después de la reasignación
+print(nombre)
+
+# Definición de una lista llamada 'lista'
+lista = ["Papa", "Gaseosa", "Hamburguesa", "Papaya"]
+
+# Acceso al elemento en la posición 2 de la lista y asignación a la variable 'elpepe'
+elpepe = lista[2]
+
+# Imprime el valor de la variable 'elpepe'
+print(elpepe)
+
+# Operaciones aritméticas en una expresión y asignación a la variable 'el_pepe'
+el_pepe = (1) + 2 + 3 + 4 * 5 + (100 * 100) / 1000
+
+# Imprime el valor de la variable 'el_pepe'
+print(el_pepe)
